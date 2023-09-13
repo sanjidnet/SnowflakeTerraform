7fad4d07298357e76af5a64d81f08196b912812b
Sanjid Rahman <sanjid.rahman@cognizant.com>
2023-09-13 14:43:00 +1200

    fix database name typo

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "prod_db_name" {
   type        = string
-  default     = "EAST_HEATLTH_PROD_DB"
+  default     = "EAST_HEALTH_PROD_DB"
   description = "Name of the production database"
 }
 
 variable "test_db_name" {
   type        = string
-  default     = "EAST_HEATLTH_TEST_DB"
+  default     = "EAST_HEALTH_TEST_DB"
   description = "Name of the test database"
 }
 
